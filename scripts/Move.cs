edec53066a63673c15217d72632b7be4ce187863
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-12-26 22:42:00 +0100

    par cucc

--- scripts/Move.cs
+++ scripts/Move.cs
@@ -3,7 +3,7 @@ using System;
 
 public class Move : KinematicBody2D
 {
-    [Export] public int speed = 5; 
+    [Export] public int speed = 200; 
     public override void _Ready()
     {
         
@@ -13,11 +13,16 @@ public class Move : KinematicBody2D
         if(Input.IsActionPressed("left")){
             moveVector.x -= 1;
         }
-        if(Input.IsActionPressed("right")){
+        else if(Input.IsActionPressed("right")){
             moveVector.x += 1;
         }
-        if(Input.IsActionPressed("jump")){
-            moveVector.y -= 1;
+        else if(Input.IsActionPressed("jump")){
+            if(IsOnFloor()){
+                moveVector.y -= 1;
+            }
+        }
+        else{
+            moveVector.y = 1f;
         }
         Position += moveVector*speed*delta;
     }
